08a73e3fa0c4aeecfa4e01144519f52fc0c44c59
Subbulakashmi <subbulakshmi.arumugam20@gmail.com>
2022-06-06 21:05:02 -0400

    Testing GITHUB.
    This can be deleted.

---
+++ app/src/main/java/com/example/projectp0/Users.kt
@@ -0,0 +1,12 @@
+package com.example.project2registration
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName="users")
+data class Users(@PrimaryKey(autoGenerate = true) var userId:Int?,
+                 @ColumnInfo(name="userName") var userName:String?,
+                 @ColumnInfo (name="password")var password:String?,
+                @ColumnInfo (name="emailId")var emailId:String?)
