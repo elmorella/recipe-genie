23415a936362c6312c2b9747c7ec1b97e7e1fcca
Elmer Orellana <elgu90@gmail.com>
2022-06-14 09:19:15 -0700

    category adapter done

--- app/src/main/java/com/example/recipegenie/viewmodel/RecipeListGenerator.kt
+++ app/src/main/java/com/example/recipegenie/viewmodel/RecipeListGenerator.kt
@@ -1,57 +1,46 @@
 package com.example.recipegenie.viewmodel
 /*
-This class generates an ArrayList<Recipe> from LiveData<List<Recipe>>
- or from MutableLiveData<RecipeResults.
+This class generates an ArrayList<Recipe> from RecipeResults
  */
-import android.text.TextUtils.lastIndexOf
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.room.ColumnInfo
-import com.example.recipegenie.R
-import com.example.recipegenie.RecipeResults
 import com.example.recipegenie.model.Recipe
+import com.example.recipegenie.model.RecipeResults
+import com.example.recipegenie.model.Results
 
 class RecipeListGenerator {
 
-    private var recipeList = ArrayList<Recipe>()
-
-    fun makeList(owner: LifecycleOwner, liveData: LiveData<List<Recipe>>?) : ArrayList<Recipe> {
-        liveData?.observe(owner) { recipeList  ->
-            this.recipeList.clear()
-            this.recipeList.addAll(recipeList)
-        }
-        return  recipeList
-    }
-
-    fun makeList(owner: LifecycleOwner, mutableLiveData: MutableLiveData<RecipeResults>)
-    : ArrayList<Recipe> {
-        mutableLiveData.observe(owner){
-            this.recipeList.clear()
-
+    fun makeList(recipeResults: RecipeResults) : ArrayList<Recipe> {
+        var recipeList = ArrayList<Recipe>()
             // create a recipe from search results
-            if(!it.results.isNullOrEmpty()) {
+            if(!recipeResults.results.isNullOrEmpty()) {
                 var recipe = Recipe(null, false,"","","",
                     "", "", "", "","")
-                for (index in 0..it.results.lastIndex) {
+
+                recipeResults.results = recipeResults.results.filter{it.sections != null} as ArrayList<Results>
+
+                for (index in 0..recipeResults.results.lastIndex) {
                     // ingredients array to String
                     var ingredients = ""
-                    for(i in 0..it.results[index].sections[0].components.lastIndex){
-                        ingredients += it.results[index].sections[0].components[i].raw_text + "\n"}
+                    for(i in 0..recipeResults.results[index].sections[0].components.lastIndex){
+                        ingredients += recipeResults.results[index]
+                            .sections[0].components[i].raw_text + "\n"}
 
                     // instructions[] to String
                     var directions = ""
-                    for(i in 0..it.results[index].instructions.lastIndex){
-                        directions += it.results[index].instructions[i].display_text + "\n"}
+                    for(i in 0..recipeResults.results[index].instructions.lastIndex){
+                        directions += recipeResults.results[index]
+                            .instructions[i].display_text + "\n"}
 
-                    var id : Int = it.results[index].id
+                    var id : Int = recipeResults.results[index].id
                     var isFavorite = false
-                    var name : String =  it.results[index].name // AKA title
-                    var yields: String = it.results[index].yields
-                    var prepTime: String = "${it.results[index].prep_time_minutes} minutes"
-                    var coockTime: String = "${it.results[index].cook_time_minutes} minutes"
-                    var totalTime: String = "${it.results[index].total_time_minutes} minutes"
-                    var imageUrl: String = it.results[index].thumbnail_url
+                    var name : String =  recipeResults.results[index].name // AKA title
+                    var yields: String = recipeResults.results[index].yields
+                    var prepTime: String = "${recipeResults.results[index]
+                        .prep_time_minutes} minutes"
+                    var coockTime: String = "${recipeResults.results[index]
+                        .cook_time_minutes} minutes"
+                    var totalTime: String = "${recipeResults.results[index]
+                        .total_time_minutes} minutes"
+                    var imageUrl: String = recipeResults.results[index].thumbnail_url
 
                     recipe.recipeId = id
                     recipe.isFavorite = isFavorite
@@ -66,7 +55,6 @@ class RecipeListGenerator {
                 }
                 recipeList.add(recipe)
             }
-        }
         return recipeList
     }
 }
