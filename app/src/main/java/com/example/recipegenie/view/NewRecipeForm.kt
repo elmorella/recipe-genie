5544f8b4afcffa13d6ba6e7cc6b9074e81e6b32c
Elmer Orellana <elgu90@gmail.com>
2022-06-13 21:29:44 -0700

    read from room db success

--- app/src/main/java/com/example/recipegenie/view/NewRecipeForm.kt
+++ app/src/main/java/com/example/recipegenie/view/NewRecipeForm.kt
@@ -16,108 +16,97 @@ class NewRecipeForm : AppCompatActivity() {
 
     lateinit var vm: MainViewModel
 
-    private lateinit var title: EditText
-    private lateinit var yields: EditText
-    private lateinit var prepTime: EditText
-    private lateinit var cookTime: EditText
-    private lateinit var ingredients: EditText
-    private lateinit var directions: EditText
+    private lateinit var inputTitle: EditText
+    private lateinit var inputYields: EditText
+    private lateinit var inputPrepTime: EditText
+    private lateinit var inputCookTime: EditText
+    private lateinit var inputIngredients: EditText
+    private lateinit var inputDirections: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_recipe_form)
 
-//        title = findViewById(R.id.edit_text_title)
-//        yields = findViewById(R.id.edit_text_yield)
-//        prepTime = findViewById(R.id.edit_text_prep_time)
-//        cookTime = findViewById(R.id.edit_text_cook_time)
-//        ingredients = findViewById(R.id.edit_text_ingredients)
-//        directions = findViewById(R.id.edit_text_directions)
+        inputTitle = findViewById(R.id.edit_text_title)
+        inputYields = findViewById(R.id.edit_text_yield)
+        inputPrepTime = findViewById(R.id.edit_text_prep_time)
+        inputCookTime = findViewById(R.id.edit_text_cook_time)
+        inputIngredients = findViewById(R.id.edit_text_ingredients)
+        inputDirections = findViewById(R.id.edit_text_directions)
 
-//        vm = MainViewModel(application)
-//
-//        val btnSubmit: ExtendedFloatingActionButton = findViewById(R.id.btn_add)
-//
-////        val btnSubmit: ExtendedFloatingActionButton = findViewById(R.id.btn_submit)
-//>>>>>>> main
-////        val btnClear: ExtendedFloatingActionButton = findViewById(R.id.btn_clear)
-//=======
-//<<<<<<< HEAD:app/src/main/java/com/example/recipegenie/NewRecipeForm.kt
-//        val btnSubmit: ExtendedFloatingActionButton = findViewById(R.id.btn_add)
-//       // val btnClear: ExtendedFloatingActionButton = findViewById(R.id.btn_clear)
-//=======
-////        val btnSubmit: ExtendedFloatingActionButton = findViewById(R.id.btn_submit)
-////        val btnClear: ExtendedFloatingActionButton = findViewById(R.id.btn_clear)
-//>>>>>>> origin:app/src/main/java/com/example/recipegenie/view/NewRecipeForm.kt
-//>>>>>>> 747e91718e6bcebec2a63430b0f83a733afc7ce0
-//        val btnCancel: ExtendedFloatingActionButton = findViewById(R.id.btn_cancel)
-//
-//
-//        btnSubmit.setOnClickListener {
-//
-//            val title: String = title.text.toString()
-//            val yields: String = yields.text.toString()
-//            val prepTime: String = prepTime.text.toString()
-//            val cookTime: String = cookTime.text.toString()
-//            val ingredients: String = ingredients.text.toString()
-//            val directions: String = directions.text.toString()
-//
-//            if (title.isNullOrBlank() || yields.isNullOrBlank() || prepTime.isNullOrBlank() ||
-//                cookTime.isNullOrBlank() || ingredients.isNullOrBlank() ||
-//                directions.isNullOrBlank()
-//            ) {
-//                if (title.isNullOrBlank())
-//                    this.title.setError("Please Enter a recipe title")
-//                if (yields.isNullOrBlank())
-//                    this.yields.setError("Please Enter number of servings")
-//                if (prepTime.isNullOrBlank())
-//                    this.prepTime.setError("Please Enter time it takes to prep ingredients")
-//                if (cookTime.isNullOrBlank())
-//                    this.cookTime.setError("Please Enter time it takes to cook")
-//                if (ingredients.isNullOrBlank())
-//                    this.ingredients.setError("Please the ingredient list")
-//                if (directions.isNullOrBlank())
-//                    this.directions.setError("Please Enter cooking steps")
-//
-//                val builder = AlertDialog.Builder(this)
-//                // Set Alert Title
-//                builder.setTitle("All fields are required")
-//                // Set the message show for the Alert time
-//                builder.setMessage("Please complete the form")
-//                builder.setCancelable(true)
-//                builder.setNegativeButton("OK", DialogInterface.OnClickListener
-//                { dialog, which -> dialog.cancel() })
-//
-//                val alertDialog: AlertDialog = builder.create()
-//                alertDialog.show()
-//
-//            } else {
-//
-//                val recipe = Recipe(
-//                    null,
-//                    true,
-//                    title,
-//                    yields,
-//                    prepTime,
-//                    cookTime,
-//                    prepTime+cookTime, //TODO: need to calculate total time
-//                    ingredients,
-//                    directions,
-//                    ""
-//                )
-//
-//                vm.insertRecipes(recipe)
-//
-//                val intent = Intent(this, FavoritesList::class.java)
-//                intent.putExtra("title", recipe.title.toString())
-//                startActivity(intent)
-//
-//                val message = "recipe successfully added"
-//                val duration = Toast.LENGTH_LONG
-//                val toast = Toast.makeText(applicationContext, message, duration)
-//                toast.show()
-//            }
-//        }
+        vm = MainViewModel(application)
+        val btnSubmit: ExtendedFloatingActionButton = findViewById(R.id.btn_add)
+        val btnCancel: ExtendedFloatingActionButton = findViewById(R.id.btn_cancel)
+
+
+        btnSubmit.setOnClickListener {
+
+            val title: String = inputTitle.text.toString()
+            val yields: String = inputYields.text.toString() + "servings"
+            val prepTime: String = inputPrepTime.text.toString() + "minutes"
+            val cookTime: String = inputCookTime.text.toString() + "minutes"
+            val totalTime: String = "${inputPrepTime.toString().toInt() +
+                                     inputCookTime.toString().toInt()} minutes"
+            val ingredients: String = inputIngredients.text.toString()
+            val directions: String = inputDirections.text.toString()
+
+            if (title.isNullOrBlank() || yields.isNullOrBlank() || prepTime.isNullOrBlank() ||
+                cookTime.isNullOrBlank() || ingredients.isNullOrBlank() ||
+                directions.isNullOrBlank()
+            ) {
+                if (title.isNullOrBlank())
+                    this.inputTitle.setError("Please Enter a recipe title")
+                if (yields.isNullOrBlank())
+                    this.inputYields.setError("Please Enter number of servings")
+                if (prepTime.isNullOrBlank())
+                    this.inputPrepTime.setError("Please Enter time it takes to prep ingredients")
+                if (cookTime.isNullOrBlank())
+                    this.inputCookTime.setError("Please Enter time it takes to cook")
+                if (ingredients.isNullOrBlank())
+                    this.inputIngredients.setError("Please the ingredient list")
+                if (directions.isNullOrBlank())
+                    this.inputDirections.setError("Please Enter cooking steps")
+
+                val builder = AlertDialog.Builder(this)
+                // Set Alert Title
+                builder.setTitle("All fields are required")
+                // Set the message show for the Alert time
+                builder.setMessage("Please complete the form")
+                builder.setCancelable(true)
+                builder.setNegativeButton("OK", DialogInterface.OnClickListener
+                { dialog, which -> dialog.cancel() })
+
+                val alertDialog: AlertDialog = builder.create()
+                alertDialog.show()
+
+            } else {
+
+
+                val recipe = Recipe(
+                    null,
+                    true,
+                    title,
+                    yields,
+                    prepTime,
+                    cookTime,
+                    totalTime,
+                    ingredients,
+                    directions,
+                    "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/178761.jpg"
+                )
+
+                vm.insertRecipes(recipe)
+
+                val intent = Intent(this, FavoritesList::class.java)
+                intent.putExtra("title", recipe.title.toString())
+                startActivity(intent)
+
+                val message = "recipe successfully added"
+                val duration = Toast.LENGTH_LONG
+                val toast = Toast.makeText(applicationContext, message, duration)
+                toast.show()
+            }
+        }
 
 //        btnCancel.setOnClickListener {
 //            val intent = Intent(this, FavoritesList::class.java)
@@ -128,44 +117,18 @@ class NewRecipeForm : AppCompatActivity() {
 //            val toast = Toast.makeText(applicationContext, message, duration)
 //            toast.show()
 //        }
-//
-//<<<<<<< HEAD
-//=======
-//<<<<<<< HEAD:app/src/main/java/com/example/recipegenie/NewRecipeForm.kt
-//            val message = "cancelled"
-//            val duration = Toast.LENGTH_LONG
-//            val toast = Toast.makeText(applicationContext, message, duration)
-//            toast.show()
-//        }
-//
-//=======
-//>>>>>>> origin:app/src/main/java/com/example/recipegenie/view/NewRecipeForm.kt
-//>>>>>>> 747e91718e6bcebec2a63430b0f83a733afc7ce0
-////        btnClear.setOnClickListener {
-////            title.text.clear()
-////            rYield.text.clear()
-////            prepTime.text.clear()
-////            totalTime.text.clear()
-////            ingredients.text.clear()
-////            directions.text.clear()
-//<<<<<<< HEAD
-////        }
-////    }
-//<<<<<<< HEAD
-//    }
-//}
-//=======
-//}
-//=======
-//<<<<<<< HEAD:app/src/main/java/com/example/recipegenie/NewRecipeForm.kt
-//        }
-//    }
-//=======
-////        }
-////    }
-//}
-//>>>>>>> origin:app/src/main/java/com/example/recipegenie/view/NewRecipeForm.kt
-//>>>>>>> 747e91718e6bcebec2a63430b0f83a733afc7ce0
-//>>>>>>> main
+
+//        val message = "cancelled"
+//        val duration = Toast.LENGTH_LONG
+//        val toast = Toast.makeText(applicationContext, message, duration)
+//        toast.show()
     }
+
+//        btnClear.setOnClickListener {
+//            title.text.clear()
+//            rYield.text.clear()
+//            prepTime.text.clear()
+//            totalTime.text.clear()
+//            ingredients.text.clear()
+//            directions.text.clear()
 }
--- app/src/main/java/com/example/recipegenie/view/RecipePage.kt
+++ app/src/main/java/com/example/recipegenie/view/RecipePage.kt
@@ -5,9 +5,10 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.recipegenie.MainActivity
 import com.example.recipegenie.R
 import com.example.recipegenie.model.RecipeRepository
-import com.example.recipegenie.UpdateRecipe
+import com.example.recipegenie.view.UpdateRecipe
 import com.example.recipegenie.model.Recipe
 import com.example.recipegenie.viewmodel.MainViewModel
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
@@ -20,76 +21,73 @@ class RecipePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_page)
+
+        val repo: RecipeRepository by lazy {
+            RecipeRepository(this)
+        }
+        vm = MainViewModel(application)
+
+        //populate TextFields with recipe data from repo and get recipeID as a String
+        var recipe: Recipe = getData(repo)
+        val btnHome: ExtendedFloatingActionButton = findViewById(R.id.btn_cancel)
+        btnHome.setOnClickListener {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnEdit: ExtendedFloatingActionButton = findViewById(R.id.btn_edit)
+        btnEdit.setOnClickListener {
+            val intent: Intent = Intent(this, UpdateRecipe::class.java)
+            intent.putExtra("title", recipe.title)
+            startActivity(intent)
+        }
+
+
+        val btnDelete: ExtendedFloatingActionButton = findViewById(R.id.btn_delete)
+        btnDelete.setOnClickListener {
+//          setOnClickListener  val builder = AlertDialog.Builder(this)
+//            // Set Alert Title
+//            builder.setTitle("Are you sure you want to delete recipe?")
+//            // Set the message show for the Alert time
+//            builder.setMessage("Hitting OK will delete the recipe")
+//            builder.setCancelable(true)
+//            builder.setNegativeButton("Cancel", DialogInterface.OnClickListener()
+//            { dialog, which -> dialog.cancel() })
+
+//            builder.setPositiveButton(
+//                "OK",DialogInterface.OnClickListener() {
 //
-//        val repo: RecipeRepository by lazy {
-//            RecipeRepository(this)
-//        }
-//        vm = MainViewModel(application)
+//             )).show()
 //
-//        //populate TextFields with recipe data from repo and get recipeID as a String
-//        var recipe: Recipe = getData(repo)
-//        val btnHome: ExtendedFloatingActionButton = findViewById(R.id.btn_cancel)
-//        btnHome.setOnClickListener {
-//            val intent: Intent = Intent(this, MainActivity::class.java)
-////        val btnHome: ExtendedFloatingActionButton = findViewById(R.id.btn_home)
-////        btnHome.setOnClickListener {
-////            val intent: Intent = Intent(this, MainActivity::class.java)
-////            startActivity(intent)
-////        }
-//
-//        val btnEdit: ExtendedFloatingActionButton = findViewById(R.id.btn_edit)
-//        btnEdit.setOnClickListener {
-//            val intent: Intent = Intent(this, UpdateRecipe::class.java)
-//            intent.putExtra("title", recipe.title)
-//            startActivity(intent)
-//        }
-//
-//
-//        val btnDelete: ExtendedFloatingActionButton = findViewById(R.id.btn_delete)
-//        btnDelete.setOnClickListener {
-////          setOnClickListener  val builder = AlertDialog.Builder(this)
-////            // Set Alert Title
-////            builder.setTitle("Are you sure you want to delete recipe?")
-////            // Set the message show for the Alert time
-////            builder.setMessage("Hitting OK will delete the recipe")
-////            builder.setCancelable(true)
-////            builder.setNegativeButton("Cancel", DialogInterface.OnClickListener()
-////            { dialog, which -> dialog.cancel() })
-//
-////            builder.setPositiveButton(
-////                "OK",DialogInterface.OnClickListener() {
-////
-////             )).show()
-////
-//
-//            vm.deleteRecipe(recipe)
-//
-//            val intent = Intent(this, FavoritesList::class.java)
-//            startActivity(intent)
-//
-//            val message = "recipe deleted"
-//            val duration = Toast.LENGTH_LONG
-//            val toast = Toast.makeText(applicationContext, message, duration)
-//            toast.show()
-//        }
-//    }
-//
-//    private fun getData(repo: RecipeRepository): Recipe {
-//        // Map TextViews in recipe page
-//        var id: TextView = findViewById(R.id.id)
-//        var title: TextView = findViewById(R.id.title)
-//        var yields: TextView = findViewById(R.id.r_yield)
-//        var prepTime: TextView = findViewById(R.id.prep_time)
-//        var totalTime: TextView = findViewById(R.id.total_time)
-//        var ingredients: TextView = findViewById(R.id.ingredients)
-//        var directions: TextView = findViewById(R.id.directions)
-//
-//        // Get recipe from repo with ID
+
+            vm.deleteRecipe(recipe)
+
+            val intent = Intent(this, FavoritesList::class.java)
+            startActivity(intent)
+
+            val message = "recipe deleted"
+            val duration = Toast.LENGTH_LONG
+            val toast = Toast.makeText(applicationContext, message, duration)
+            toast.show()
+        }
+    }
+
+    private fun getData(repo: RecipeRepository): Recipe {
+        // Map TextViews in recipe page
+        var id: TextView = findViewById(R.id.id)
+        var title: TextView = findViewById(R.id.title)
+        var yields: TextView = findViewById(R.id.r_yield)
+        var prepTime: TextView = findViewById(R.id.prep_time)
+        var totalTime: TextView = findViewById(R.id.total_time)
+        var ingredients: TextView = findViewById(R.id.ingredients)
+        var directions: TextView = findViewById(R.id.directions)
+
+        // Get recipe from repo with ID
 //        var recipe: List<Recipe> =
 //            repo.findRecipeWithTitle(intent.getStringExtra("title").toString())
-//
-//        //TODO: Null validation
-//
+
+        //TODO: Null validation
+
 //        // Populate Text Views with recipe fields
 //        id.text = recipe[0].recipeId.toString()
 //        title.text = recipe[0].title
@@ -98,13 +96,13 @@ class RecipePage : AppCompatActivity() {
 //        totalTime.text = recipe[0].totalTime
 //        ingredients.text = recipe[0].ingredients
 //        directions.text = recipe[0].directions
-//
-//        //TODO need to calculate cook time and URL
-//
-//        return Recipe(
-//            id.text.toString().toInt(), false, title.text.toString(), yields.text.toString(),
-//            prepTime.text.toString(),"Need to calculate", totalTime.text.toString(),
-//            ingredients.text.toString(), directions.text.toString(), "get URL"
-//        )
+
+        //TODO need to calculate cook time and URL
+
+        return Recipe(
+            id.text.toString().toInt(), false, title.text.toString(), yields.text.toString(),
+            prepTime.text.toString(),"Need to calculate", totalTime.text.toString(),
+            ingredients.text.toString(), directions.text.toString(), "get URL"
+        )
     }
 }
